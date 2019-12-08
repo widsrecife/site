c0eaff136573881258005980b2a44227d561b37e
cecivieira <cecivieira@gmail.com>
2019-12-07 22:00:29 -0300

    Add component NextEvents

---
+++ src/components/NextEvents/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './next-events.css';
+
+
+const NextEvents = () => ( 
+    <section>
+        <h1>Próximos Eventos</h1>
+
+        
+    </section>
+);
+
+export default NextEvents;
