a21a63f5e1b5595e197620c7a8d408ee3f4da1f1
cecivieira <cecivieira@gmail.com>
2019-12-07 22:11:14 -0300

    Update main index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import './app.css';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import './i18n';
 
-import Header from '../Header'
-import NextEvents from '../NextEvents';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import 'bulma/css/bulma.css'
-import divisor from '../../assets/img/site-divisor-svg.svg'
-
-const App = () => (
-  <div className="container">
-    <Header />
-    <img class="divisor"src={divisor}></img>
-    <NextEvents />
-  </div>
-)
-
-export default App;
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
