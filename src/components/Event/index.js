cc4be9cecaa50d664b1b65ad7ad50d424204c197
cecivieira <cecivieira@gmail.com>
2019-12-08 19:18:08 -0300

    Add event component

---
+++ src/components/Event/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './event.css';
+
+const Event = () => ( 
+    <section>
+
+    </section>
+);
+
+export default Event;
--- src/components/NextEvents/index.js
+++ src/components/NextEvents/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './next-events.css';
-
+import Event from '../Event';
 
 const NextEvents = () => ( 
     <section>
         <h1>Próximos Eventos</h1>
+        <Event />
 
         
     </section>
