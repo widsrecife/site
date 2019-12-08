77341b1f483cdaea109ac95601ff5c0cf17d97c4
cecivieira <cecivieira@gmail.com>
2019-12-08 20:02:08 -0300

    Add conduct code component issue #7

---
+++ src/components/ConductCode/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './conductCode.css';
+
+import Header from '../Header'
+import Footer from '../Footer';
+
+import '../../assets/lib/bulma.css'
+import divisor from '../../assets/img/site-divisor-svg.svg'
+import poster from '../../assets/img/img-cond-conduta.png'
+
+const ConductCode = () => (
+  <div className="container">
+    <Header poster={poster} />
+    <h1>Código de Conduta</h1>
+    <img className="divisor" src={divisor} alt="Formas geométricas" />
+    <p>Escreva aqui</p>
+    <Footer />
+  </div>
+)
+
+export default ConductCode;
