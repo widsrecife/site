cebc884866b29a098a5a25c9cb108b3de4c76a44
cecivieira <cecivieira@gmail.com>
2019-12-07 23:06:23 -0300

    Add footer structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import './app.css';
 
 import Header from '../Header'
 import NextEvents from '../NextEvents';
+import Footer from '../Footer';
 
 import '../../assets/lib/bulma.css'
 import divisor from '../../assets/img/site-divisor-svg.svg'
@@ -12,6 +13,7 @@ const App = () => (
     <Header />
     <img className="divisor" src={divisor} alt="Formas geométricas" />
     <NextEvents />
+    <Footer />
   </div>
 )
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => (
+    <section className="footer">
+        Footer
+    </section>
+);
+
+export default Footer;
