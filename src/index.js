82437e996b0b53942c4762df45072af38816826a
cecivieira <cecivieira@gmail.com>
2019-12-07 21:56:51 -0300

    Add component App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './app.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Header from '../Header'
+import NextEvents from '../NextEvents';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import 'bulma/css/bulma.css'
+import divisor from '../../assets/img/site-divisor-svg.svg'
+
+const App = () => (
+  <div className="container">
+    <Header />
+    <img class="divisor"src={divisor}></img>
+    <NextEvents />
+  </div>
+)
+
+export default App;
