becb463ec9ada9d0a763cabac7ca5dc6af348706
cecivieira <cecivieira@gmail.com>
2019-12-08 12:32:37 -0300

    Edit header properties image

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,10 +8,11 @@ import InviteTalk from '../InviteTalk'
 
 import '../../assets/lib/bulma.css'
 import divisor from '../../assets/img/site-divisor-svg.svg'
+import poster from '../../assets/img/poster-home4.jpg'
 
 const App = () => (
   <div className="container">
-    <Header />
+    <Header poster={poster} />
     <img className="divisor" src={divisor} alt="Formas geométricas" />
     <NextEvents />
     <InviteTalk />
