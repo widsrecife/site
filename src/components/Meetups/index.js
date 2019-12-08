0cdf94693ebf2abe187e199cf7447a683d46041e
cecivieira <cecivieira@gmail.com>
2019-12-08 20:50:34 -0300

    Add meetups component #9

---
+++ src/components/Meetups/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './meetups.css';
+
+import Header from '../Header'
+import Footer from '../Footer';
+
+import '../../assets/lib/bulma.css'
+import divisor from '../../assets/img/site-divisor-svg.svg'
+import poster from '../../assets/img/img-meetup.png'
+
+const Meetups = () => (
+  <div className="container">
+    <Header poster={poster} />
+    <h1>Meetups</h1>
+    <img className="divisor" src={divisor} alt="Formas geométricas" />
+    <Event />
+    <Footer />
+  </div>
+)
+
+export default Meetups;
