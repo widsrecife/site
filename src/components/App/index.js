d2cf0a022b1bc1fec119952f2f34fb4c4c5c4c43
Talitha Medeiros <medeirostalitha@gmail.com>
2020-02-27 17:38:47 -0300

    build

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,20 +4,16 @@ import './app.css';
 import NextEvents from '../NextEvents';
 import Footer from '../Footer';
 import About from '../About';
-import Meetups from '../Meetups';
 
 import '../../assets/lib/bulma.css';
 import divisor from '../../assets/img/site-divisor-svg.svg';
 
 const App = () => (
   <div className="container-default" id="index">
-    {/* <About />
+    <About />
     <img className="divisor" src={divisor} alt="Formas geométricas" />
     <NextEvents />
-     */}
-
-     <Meetups />
-
+    
     <Footer />
   </div>
 )
